608e380ec06c2080810ed87dd274ca8a26530ddf
Anderson Rodrigues <maikbru@gmail.com>
2021-04-25 13:36:01 -0300

    auth

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$2zlgpV3T8clNzQbiAyOb4O9rFucZERGVLBE1FpZhQ91892NUGzQyS');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
