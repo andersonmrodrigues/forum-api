8fb2cf718718b48540a5a69e1be1b46da40c69ef
Anderson Rodrigues <maikbru@gmail.com>
2021-04-26 21:12:25 -0300

    jwt auth

---
+++ src/main/java/br/com/desenv/api/config/TokenService.java
@@ -0,0 +1,32 @@
+package br.com.desenv.api.config;
+
+import br.com.desenv.api.model.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String generate(Authentication authentication) {
+        Usuario usuario = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        Date exp = new Date(hoje.getTime() + Long.parseLong(expiration));
+        return Jwts.builder()
+                .setIssuer("Forum API")
+                .setSubject(usuario.getId().toString())
+                .setIssuedAt(hoje)
+                .setExpiration(exp)
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+}
